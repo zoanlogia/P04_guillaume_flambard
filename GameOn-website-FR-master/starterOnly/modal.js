28230e11bdf3a0dca5dd6bc9350434d0ba3fdc45
Guillaume Flambard <g.flambard@gmail.com>
2021-10-30 13:12:16 +0200

    add error messages

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -7,107 +7,135 @@ function editNav() {
   }
 }
 
+// ===============================
+//         DECLARATIONS
+// ===============================
 // DOM Elements
-modalbg = document.querySelector(".bground");
-modalBtn = document.querySelectorAll(".modal-btn");
-form = document.getElementById('form');
-formData = document.querySelectorAll(".formData");
-closeBtn = document.getElementsByClassName('close');
-firstName = document.getElementById("first");
-lastName = document.getElementById("last");
-email = document.getElementById('email');
-birthdate = document.getElementById('birthdate');
-quantity = document.getElementById('quantity');
-locations = document.getElementsByName('location')
-regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-checkbox1 = document.getElementById('checkbox1');
-checkbox2 = document.getElementById('checkbox2');
-textControl = document.getElementsByClassName('text-control');
-firstNameValue = firstName.value.trim()
-lastNameValue = lastName.value.trim()
-emailValue = email.value.trim()
-birthdateValue = birthdate.value.trim()
-quantityValue = quantity.value.trim()
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const form = document.getElementById('form');
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
-// launch modal event
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const locations = document.getElementsByName('location')
 
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
+const textControl = document.getElementsByClassName('text-control');
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // close modal form
-
 function hide() {
   modalbg.style.display = "none";
 }
 
-// on click outside the modal, the modal close
+const STATE = {
+  name: '',
+  lastname: '',
+  email: '',
+  birthdate: '',
+  qtty: '',
+  location: '',
+  cgu: true,
+  newsLetter: false
+}
 
+const initState = () => {
+  STATE.name = firstName.value
+  STATE.lastname = lastName.value
+  STATE.email = email.value
+  STATE.birthdate = birthdate.value
+  STATE.qtty = quantity.value
+  STATE.location = locations
+  STATE.cgu = checkbox1.checked
+  STATE.newsLetter = checkbox2.checked
+}
+
+const isValidDOM = element => {
+  element.parentElement.classList.remove('error');
+  element.parentElement.classList.add('success');
+  element.parentElement.setAttribute('data-error-visible', 'false');
+}
+
+const isNotValidDOM = element => {
+  element.parentElement.classList.remove('success');
+  element.parentElement.classList.add('error');
+  element.parentElement.setAttribute('data-error-visible', 'true');
+}
+
+const validation = () => {
+
+  STATE.name.length > 2 ? isValidDOM(firstName) : isNotValidDOM(firstName)
+
+  STATE.lastname.length > 2 ? isValidDOM(lastName) : isNotValidDOM(lastName)
+
+  STATE.email.match(regex) && STATE.email !== "" ? isValidDOM(email) : isNotValidDOM(email)
+
+  STATE.birthdate !== "" ? isValidDOM(birthdate) : isNotValidDOM(birthdate)
+
+  STATE.qtty > 0 && STATE.qtty !== "" ? isValidDOM(quantity) : isNotValidDOM(quantity)
+
+  if (STATE.cgu == true) {
+    formData[6].classList.remove('error')
+    formData[6].classList.add('success')
+    formData[6].setAttribute('data-error-visible', 'false')
+  } else {
+    formData[6].classList.remove('success')
+    formData[6].classList.add('error')
+    formData[6].setAttribute('data-error-visible', 'true')
+  }
+
+  if (STATE.newsLetter == true && STATE.newsLetter == false) {
+    formData[6].classList.remove('error')
+    formData[6].classList.add('success')
+    formData[6].setAttribute('data-error-visible', 'false')
+  }
+
+  for (var i = 0; i < STATE.location.length; i++) {
+    if (STATE.location[i].checked == true) {
+      formData[5].classList.remove('error')
+      formData[5].classList.add('success')
+      formData[5].setAttribute('data-error-visible', 'false')
+      return true
+    } else {
+      formData[5].classList.add('error');
+      formData[5].classList.remove('success')
+      formData[5].setAttribute('data-error-visible', 'true')
+      return false
+    }
+  }
+}
+
+
+
+
+
+// ===============================
+//            INIT
+// ===============================
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// on click outside the modal, the modal close
 window.onclick = function (e) {
   if (e.target == modalbg) {
     modalbg.style.display = "none";
   }
 }
 
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  validate()
-  // return confirm('heeeyyy')
+  initState()
+  validation()
 })
-
-function validate() {
-
-  if (firstNameValue === '' || firstNameValue < 2) {
-    formData[0].setAttribute('data-error-visible', 'true');
-    return false
-  } else {
-    formData[0].setAttribute('data-error-visible', 'false');
-    textControl[0].style.border = "3px solid #31b531"
-  }
-  if (lastNameValue === '' || lastNameValue < 2) {
-    formData[1].setAttribute('data-error-visible', 'true');
-    return false
-  } else {
-    formData[1].setAttribute('data-error-visible', 'false');
-    textControl[1].style.border = "3px solid #31b531"
-  }
-  if (emailValue == emailValue.match(regex) || emailValue == "") {
-    formData[2].setAttribute('data-error-visible', 'true');
-    return false
-  } else {
-    formData[2].setAttribute('data-error-visible', 'false');
-    textControl[2].style.border = "3px solid #31b531"
-  }
-  if (birthdateValue == '') {
-    formData[3].setAttribute('data-error-visible', 'true');
-    return false
-  } else {
-    formData[3].setAttribute('data-error-visible', 'false');
-    textControl[3].style.border = "3px solid #31b531"
-  }
-  if (isNaN(quantityValue) || quantityValue == '' || quantityValue <= 0 || quantityValue > 99) {
-    formData[4].setAttribute('data-error-visible', 'true');
-    return false
-  } else {
-    formData[4].setAttribute('data-error-visible', 'false');
-    textControl[4].style.border = "3px solid #31b531"
-  }
-  if (checkbox1.checked !== true) {
-    formData[6].setAttribute('data-error-visible', 'true');
-    return false
-  } else {
-    formData[6].setAttribute('data-error-visible', 'false');
-  }
-  for (var i = 0; i < locations.length; i++) {
-    if (locations[i].checked == false) {
-      formData[5].setAttribute('data-error-visible', 'true');
-      return false
-    } else {
-      formData[5].setAttribute('data-error-visible', 'false');
-      return true
-    }
-  }
-}
